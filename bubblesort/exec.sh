9c0d6d099d992b098fe552d1576747835b6bb8c1
Bruno Ferreira Coelho <bfc1@outlook.com>
2019-06-20 18:19:55 -0300

    oi dnv

--- bubblesort/exec.sh
+++ bubblesort/exec.sh
@@ -8,14 +8,14 @@ echo 0 > /proc/sys/kernel/nmi_watchdog
 #   sudo perf stat -e L1-dcache-load-misses,l2_rqsts.code_rd_miss,LLC-load-misses,branches,branch-misses,task-clock,cycles,instructions ./a.out $i 2> $i.txt
 # done
 
-for i in $(seq 1001 1000 50001);
+for i in $(seq 1000 1000 50000);
 do
   sudo perf stat -e L1-dcache-load-misses,l2_rqsts.miss,LLC-load-misses,branches,branch-misses,task-clock,cycles,instructions ./a.out $i 2> results/$i.txt
 done
 
 echo 1 > /proc/sys/kernel/nmi_watchdog
 
-for i in $(seq 1001 1000 50001);
+for i in $(seq 1000 1000 50000);
 do
   chmod 777 results/$i.txt
 done
