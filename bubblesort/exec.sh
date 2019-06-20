12bae8cb278c786160b5855249bdb3c3a203070d
Gabriel Martins <gmbiel1@gmail.com>
2019-06-20 20:55:19 +0000

    [fix] Fixed l2_rqsts.miss

--- bubblesort/exec.sh
+++ bubblesort/exec.sh
@@ -10,7 +10,7 @@ echo 0 > /proc/sys/kernel/nmi_watchdog
 
 for i in $(seq 1 1000 50001);
 do
-  sudo perf stat -e L1-dcache-load-misses,l2_rqsts.code_rd_miss,LLC-load-misses,branches,branch-misses,task-clock,cycles,instructions ./a.out $i 2> $i.txt
+  sudo perf stat -e L1-dcache-load-misses,l2_rqsts.miss,LLC-load-misses,branches,branch-misses,task-clock,cycles,instructions ./a.out $i 2> $i.txt
 done
 
 echo 1 > /proc/sys/kernel/nmi_watchdog
