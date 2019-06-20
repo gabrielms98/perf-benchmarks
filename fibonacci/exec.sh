0a04bf57dc0ba28113ead10ce2915e29306dcd56
Gabriel Martins <gmbiel1@gmail.com>
2019-06-20 20:42:10 +0000

    [fix] Fixing perf call from l2_rqsts.code_rd_miss to l2_rqsts.miss and redo of tests

--- fibonacci/exec.sh
+++ fibonacci/exec.sh
@@ -5,7 +5,7 @@
 echo 0 > /proc/sys/kernel/nmi_watchdog
 
 for i in $(seq 20 2 46) ; do
-  sudo perf stat -e L1-dcache-load-misses,l2_rqsts.code_rd_miss,LLC-load-misses,branches,branch-misses,task-clock,cycles,instructions ./a.out $i 2> results/$i.txt
+  sudo perf stat -e L1-dcache-load-misses,l2_rqsts.miss,LLC-load-misses,branches,branch-misses,task-clock,cycles,instructions ./a.out $i 2> results/$i.txt
 done
 
 echo 1 > /proc/sys/kernel/nmi_watchdog
