4f7d7bcb14cedb2a08bb390dd289ef5430d3e8c9
Bruno Ferreira Coelho <bfc1@outlook.com>
2019-06-20 21:04:55 -0300

    lista valgrind

--- lista/valgrind.sh
+++ lista/valgrind.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-valgrind --tool=cachegrind --I1=1,1, ./a.out 100 10 50
+for c in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152;
+do
+	valgrind --tool=cachegrind \
+		--I1=$c,4,128 \
+		--D1=$c,4,128 \
+		--LL=$c,4,128 \
+		./a.out 5000 100 10000 2> results/$c.txt
+done
