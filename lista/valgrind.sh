a6f43e606b1355d40c8015b26abdef21060b8214
Bruno Ferreira Coelho <bfc1@outlook.com>
2019-06-24 15:39:13 -0300

    [fix] lista miss rate

--- lista/valgrind.sh
+++ lista/valgrind.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-for c in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152;
+for c in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304;
 do
 	valgrind --tool=cachegrind \
 		--I1=$c,4,128 \
 		--D1=$c,4,128 \
 		--LL=$c,4,128 \
-		./a.out 5000 100 10000 2> results/$c.txt
+		./a.out 10000 100 10000 2> results/$c.txt
+	
+	echo $c finalizada
 done
+
+rm cachegrind.out.*
