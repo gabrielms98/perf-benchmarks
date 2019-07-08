3f161118877a739f77f7778c205d53f5345a4313
RicsonL <ricsonluiz@yahoo.com.br>
2019-07-07 23:50:18 -0300

    [fodase] chega

--- etapa1 e 2/perf.py
+++ etapa1 e 2/perf.py
@@ -7,26 +7,22 @@ import os
 
 if sys.argv[3] == 'L1':
     cache = 3
-elif sys.argv[3] == 'L2':
-    cache = 4
 elif sys.argv[3] == 'L3':
     cache = 5
 
 dict = {}
-files = sorted(os.listdir(sys.argv[1]),key=lambda x: int(x[x.find('-')+1 : x.find('.')]))
+files = sorted(os.listdir(sys.argv[1]),key=lambda x: int(x.split('.')[0]))
+print(files)
 line = []
 for i in (files):
     with open(sys.argv[1]+'/'+i) as f:
         lines = f.readlines()
-        num = lines[cache].split()[0].split('.')
-        instr = lines[10].split()[0].split('.')
-        totalN = ''
-        totalInstr = ''
-        for p in num:
-            totalN+=p
-        for p in instr:
-            totalInstr+=p
-        line.append((int(totalN)/int(totalInstr))*100)
+        ppp = lines[cache].split()
+        perc = '0'
+        if len(ppp)==3:
+            for p in ppp[2][1:len(ppp[2])-2].split(','):
+                perc+=p
+        line.append(int(perc)/100)
     dict[sys.argv[3]] = line
 for i in range(0,len(files)):
     files[i] = int(files[i].split('.')[0])
